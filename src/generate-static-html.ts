2c57aefd1cd61e4367a068108b250ac8805f47fb
Scottgemma <scottgemma@Scottgemmas-MacBook-Pro.local>
2023-06-30 18:16:30 -0400

    backed in ale tweaks

--- src/generate-static-html.ts
+++ src/generate-static-html.ts
@@ -1,6 +1,8 @@
 import { getState, clearStore } from './state';
 import { generateMetasMarkup } from './render/static';
 
+const LINK_PRELOAD = 'link_rel=preload~';
+
 // Function to generate metas static HTML
 export const generateStaticHtml = () => {
   const metas = getState();
@@ -8,4 +10,22 @@ export const generateStaticHtml = () => {
 };
 export const resetMetaTags = () => {
   clearStore();
+};
+export const generateStaticHtmlHighPriority = () => {
+  const { tags, title } = getState();
+
+  const highPriorityTags = Object.keys(tags)
+    .filter((key) => key.startsWith(LINK_PRELOAD))
+    .reduce((cur, key) => Object.assign(cur, { [key]: tags[key] }), {});
+
+  return generateMetasMarkup({ tags: highPriorityTags, title });
+};
+export const generateStaticHtmlLowPriority = () => {
+  const { tags, title, ...all } = getState();
+
+  const lowPriorityTags = Object.keys(tags)
+    .filter((key) => !key.startsWith(LINK_PRELOAD))
+    .reduce((cur, key) => Object.assign(cur, { [key]: tags[key] }), {});
+
+  return generateMetasMarkup({ ...all, tags: lowPriorityTags });
 };
